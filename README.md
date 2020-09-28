ec4baec4bc40868802ee4521308ce7df15586471
Thach Nguyen <nthachus@gmail.com>
2020-09-28 19:44:55 +0700

    Use `@babel/register` to run unit-tests on nodejs v6

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ Updates all packages in `yarn.lock` file safety.
 
 Install dependencies:
 
-    $ docker run --rm -it -v "%PWD%:/usr/src/app" -w /usr/src/app node:10-alpine yarn --ignore-optional --no-bin-links
+    $ docker run --rm -it -v "%PWD%:/usr/src/app" -w /usr/src/app node:10-alpine yarn --ignore-optional --ignore-engines --no-bin-links
 
 Test and run:
 
     $ docker run --rm -it -v "%PWD%:/usr/src/app" -w /usr/src/app node:10-alpine /bin/sh -c "yarn format -c && yarn lint && yarn test"
-    $ docker run --rm -it -v "%PWD%:/usr/src/app" -w /usr/src/app node:10-alpine /bin/sh -c "yarn build && node dist/cli.js"
+    $ docker run --rm -it -v "%PWD%:/usr/src/app" -w /usr/src/app node:10-alpine /bin/sh -c "yarn build && node dist/cli.js test/fixtures/yarn.lock"
 
 **NOTE** the `%PWD%` is the project working directory, like: `/c/Users/xxx/repo/yarn-safe-update`
--- test/cli.js
+++ test/cli.js
@@ -3,25 +3,26 @@ const { execFileSync } = require('child_process');
 const assert = require('assert');
 
 const cliFilePath = path.resolve(__dirname, '../src/cli.js');
+const executeCli = (...args) => execFileSync(process.execPath, [cliFilePath, ...args]);
 
 describe('CLI', () => {
   ['-v', '--version'].forEach((arg) => {
     it(`prints version with option '${arg}'`, () => {
-      const out = execFileSync(process.execPath, [cliFilePath, arg]);
+      const out = executeCli(arg);
       assert(/^\d+(\.\d+)*\s*$/.test(out), out);
     });
   });
 
   ['-h', '--help'].forEach((arg) => {
     it(`prints help with option '${arg}'`, () => {
-      const out = execFileSync(process.execPath, [cliFilePath, arg]);
+      const out = executeCli(arg);
       assert(/^Usage: cli \[options] \[yarn\.lock path\b.*?]/.test(out), out);
     });
   });
 
   it('fails if given an invalid option', () => {
     assert.throws(
-      () => execFileSync(process.execPath, [cliFilePath, '--fail']),
+      () => executeCli('--fail'),
       (e) => e instanceof Error && e.status === 1 && /\bUnknown option\b/i.test(e)
     );
   });
